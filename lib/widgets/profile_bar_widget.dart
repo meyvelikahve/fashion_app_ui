6bf6bd19873ce97cdb9d920fda55296352fed4b4
meyvelikahve <recep_sevim20@hotmail.com>
2021-10-20 14:33:03 +0300

    first_commit

---
+++ lib/widgets/profile_bar_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileBar extends StatelessWidget {
+  const ProfileBar({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
